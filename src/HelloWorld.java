954932ce03625c83f96c5303848d09f214390ece
g17959yr <g17959yr@gm.tsuda.ac.jp>
2019-04-12 17:48:09 +0900

    メッセージを表示するプログラムを作成しました。

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello,World!");
+		System.out.println("ソフトウェア工学についての理解を");
+	    System.out.println( "ひとつひとつ進めていこうと思います。");
+	}
+
+}
